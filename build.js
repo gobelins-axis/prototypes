1e7b6ecab5900568e0c7e8db53f1729e81ff404e
leochocolat <l.mouraire@outlook.fr>
2022-02-03 23:27:20 +0100

    :green_heart: Improve build script

--- build.js
+++ build.js
@@ -1,37 +1,86 @@
 // Utils
+const fs = require('fs');
 const yargs = require('yargs');
+const md5 = require('md5');
 const { exec } = require('child_process');
 
 // Config
 require('dotenv').config();
 
 // Request options
-const { name } = yargs
+const options = yargs
     .usage('Usage: --name <name>')
     .option('n', { alias: 'name', describe: 'Sketch name', type: 'string', demandOption: true })
+    .option('w', { alias: 'watch', describe: 'Watch file changes', type: 'bool', demandOption: false })
     .argv;
 
 const port = process.env.PORT;
 const fqbn = process.env.FQBN;
-const sketch = name;
+const sketch = options.name;
+const sketchPath = `./${sketch}/${sketch}.ino`;
 
 const commandCompile = `arduino-cli compile --fqbn ${fqbn} ${sketch}`;
 const commandUpload = `arduino-cli upload -p ${port} --fqbn ${fqbn} ${sketch}`;
 
-exec(commandCompile, (compileError, compileResponse) => {
-    if (compileError) {
-        console.error(compileError);
-        return;
-    };
+let sketchContent = md5(fs.readFileSync(sketchPath));
+let allowCompile = true;
 
-    console.log(`Compile success: ${compileResponse}`);
+function setup() {
+    compileAndUpload();
 
-    exec(commandUpload, (uploadError, uploadResponse) => {
-        if (uploadError) {
-            console.error(uploadError);
+    if (options.watch) watch();
+}   
+
+function watch() {
+    fs.watch(sketchPath, fileChangeHandler);
+}
+
+function fileChangeHandler(event, filename) {
+    if (!allowCompile || !filename || event !=='change') return;
+
+    const newSketchContent = md5(fs.readFileSync(sketchPath));
+
+    // Check if file content is newer to prevent 
+    // compiling on save but no changes
+    if (newSketchContent !== sketchContent) {
+        compileAndUpload();
+    }
+    
+    sketchContent = newSketchContent;
+}
+
+function compileAndUpload() {
+    if (!allowCompile) return;
+
+    console.log('⏳ Compiling...');
+    
+    exec(commandCompile, (compileError, compileResponse) => {
+
+        if (compileError) {
+            console.error('❌ Compiling Failed');
+            console.error(compileError);
             return;
         };
+    
+        console.log(`✅ Compile success`);
+        console.log(compileResponse);
 
-        console.log(`Upload success: ${uploadResponse}`);
+        console.log('⏳ Uploading...');
+    
+        exec(commandUpload, (uploadError, uploadResponse) => {
+
+            if (uploadError) {
+                console.error('❌ Uploading Failed');
+                console.error(uploadError);
+                return;
+            };
+
+            allowCompile = true;
+    
+            console.log(`✅ Upload success!`);
+            console.log(uploadResponse);
+        });
     });
-});
+}
+
+setup();
